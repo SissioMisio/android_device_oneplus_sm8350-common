436782626b71f3fe2d6066b58e6ad8141ac689d0
SissioMisio <40896232+SissioMisio@users.noreply.github.com>
2023-09-02 19:22:56 +0200

    Rising bringup

--- common.mk
+++ common.mk
@@ -296,7 +296,7 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-evolution
+    $(LOCAL_PATH)/overlay-rising
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
